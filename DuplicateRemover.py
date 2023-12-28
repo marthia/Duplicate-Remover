cd9c81a0ed4e407e8b27bfbd4403341f5d785efb
Marthia <55075092+marthia@users.noreply.github.com>
2023-12-28 23:05:09 +0330

    Update DuplicateRemover.py
    take file size into account when deciding to delete the duplicate images, keep the largest and delete the rest
    
    instead of removing images permanently, use send2trash which safer.

--- DuplicateRemover.py
+++ DuplicateRemover.py
@@ -2,6 +2,7 @@ from PIL import Image
 import imagehash
 import os
 import numpy as np
+import send2trash
 
 class DuplicateRemover:
     def __init__(self,dirname,hash_size = 8):
@@ -16,13 +17,26 @@ class DuplicateRemover:
         fnames = os.listdir(self.dirname)
         hashes = {}
         duplicates = []
+
+
+
         print("Finding Duplicates Now!\n")
         for image in fnames:
             with Image.open(os.path.join(self.dirname,image)) as img:
                 temp_hash = imagehash.average_hash(img, self.hash_size)
                 if temp_hash in hashes:
+                    
                     print("Duplicate {} \nfound for Image {}!\n".format(image,hashes[temp_hash]))
-                    duplicates.append(image)
+                    
+                    if (os.path.getsize(os.path.join(self.dirname,hashes[temp_hash])) < os.path.getsize(os.path.join(self.dirname,image))) :
+                        # remove the old higher quality photo from duplicates
+                        if image in duplicates : duplicates.remove(image)
+                        # add the lower quality photo to duplicates
+                        duplicates.append(hashes[temp_hash])
+                        # replace the image with higher size with the older one
+                        hashes[temp_hash] = image
+                    else:
+                        duplicates.append(image)
                 else:
                     hashes[temp_hash] = image
                    
@@ -33,7 +47,8 @@ class DuplicateRemover:
                 for duplicate in duplicates:
                     space_saved += os.path.getsize(os.path.join(self.dirname,duplicate))
                     
-                    os.remove(os.path.join(self.dirname,duplicate))
+                    # os.remove(os.path.join(self.dirname,duplicate))
+                    send2trash.send2trash(os.path.join(self.dirname,duplicate))
                     print("{} Deleted Succesfully!".format(duplicate))
     
                 print("\n\nYou saved {} mb of Space!".format(round(space_saved/1000000),2))
@@ -41,10 +56,7 @@ class DuplicateRemover:
                 print("Thank you for Using Duplicate Remover")
         else:
             print("No Duplicates Found :(")
-            
         
-            
-            
     def find_similar(self,location,similarity=80):
         fnames = os.listdir(self.dirname)
         threshold = 1 - similarity/100
@@ -59,10 +71,6 @@ class DuplicateRemover:
                 hash2 = imagehash.average_hash(img, self.hash_size).hash
                 
                 if np.count_nonzero(hash1 != hash2) <= diff_limit:
-                    print("{} image found {}% similar to {}".format(image,similarity,location))
-                    
-                    
-                    
-                
-        
-            
+                    print("{} image found {}% similar to {} and Succesfully Deleted".format(image,similarity,location))
+                    # send2trash.send2trash(os.path.join(self.dirname,image))
+
